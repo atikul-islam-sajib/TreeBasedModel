91c52be53864e77cb176651493d59b55feab8987
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-07-26 01:36:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='AugmentedHierarchicalShrinkage',
-      version="1",
-      description="Python package for master thesis",
-      packages=find_packages(),
-      install_requires=requirements,
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/Run_PredPerf_experiment'],
-      zip_safe=False)
+setup(
+    name='AugmentedHierarchicalShrinkage',
+    version="1",
+    description="Python package for master thesis",
+    packages=find_packages(),
+    install_requires=requirements,
+    # include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    zip_safe=False
+)
